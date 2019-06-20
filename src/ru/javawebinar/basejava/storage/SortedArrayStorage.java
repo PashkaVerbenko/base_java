160c6acdaa76e864438e6f68f25f734fbe1d71d2
PashkaVerbenko <pashka_verbenko@ukr.net>
2019-06-20 19:58:46 +0300

    Lesson03 abstract storage & sorted storage

--- src/ru/javawebinar/basejava/storage/SortedArrayStorage.java
+++ src/ru/javawebinar/basejava/storage/SortedArrayStorage.java
@@ -4,30 +4,54 @@ import ru.javawebinar.basejava.model.Resume;
 
 import java.util.Arrays;
 
-public class SortedArrayStorage extends AbstractArrayStorage{
+public class SortedArrayStorage extends AbstractArrayStorage {
     @Override
     public void clear() {
-
+        Arrays.fill(storage, 0, size, null);
+        size = 0;
     }
 
     @Override
     public void update(Resume r) {
-
+        int index = getIndex(r.getUuid());
+        if (index == -1) {
+            System.out.println("Resume " + r.getUuid() + " not exist");
+        } else {
+            storage[index] = r;
+        }
     }
 
     @Override
     public void save(Resume r) {
-
+        if (getIndex(r.getUuid()) != -1) {
+            System.out.println("Resume " + r.getUuid() + " already exist");
+        } else if (size >= STORAGE_LIMIT) {
+            System.out.println("Storage overflow");
+        } else {
+            //storage[size] = r;
+            size++;
+        }
     }
 
     @Override
     public void delete(String uuid) {
-
-    }
+        int index = getIndex(uuid);
+        if (index == -1) {
+            System.out.println("Resume " + uuid + " not exist");
+        } else {
+            storage[index] = null;
+            for (int i = 0; i < size; i++) {
+                storage[i] = storage[i+1];
+                    }
+                }
+            //storage[index] = storage[size - 1];
+            storage[size - 1] = null;
+            size--;
+        }
 
     @Override
     public Resume[] getAll() {
-        return new Resume[0];
+        return Arrays.copyOfRange(storage, 0, size);
     }
 
     @Override
